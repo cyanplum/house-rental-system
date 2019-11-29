5a429cee80bb66b3c1fb79670735ae4441c1c074
Aquarius <1309284266@qq.com>
2019-11-29 17:26:40 +0800

    fix bug

--- src/main/java/house/rental/system/controller/LoginController.java
+++ src/main/java/house/rental/system/controller/LoginController.java
@@ -30,11 +30,7 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
-
-    @GetMapping
-    public Response index(){
-        return Response.success();
-    }
+    
 
     @PostMapping("/registered")
     public JSONResult registered(@RequestBody UserInfo userInfo){
